a664a0ee20eb9ba4885ac9a604402b093663cc22
Yvo van Zee <yvo.vanzee@xebia.com>
2024-04-22 16:05:24 +0200

    using projen pipelines

---
+++ src/buildinginthecloud_stack.ts
@@ -0,0 +1,23 @@
+import { Stack, StackProps, aws_route53, aws_route53_patterns } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+
+export interface BuildingInTheCloudStackProps extends StackProps {
+    hostedZoneName: string
+    targetDomainName: string
+  }
+
+export class BuildingInTheCloudStack extends Stack {
+    constructor(scope: Construct, id: string, props: BuildingInTheCloudStackProps) {
+      super(scope, id, props);
+  
+      const HostedZone = new aws_route53.HostedZone(this, 'HostedZone', {
+        zoneName: props.hostedZoneName,
+      });
+  
+      new aws_route53_patterns.HttpsRedirect(this, 'Redirect', {
+        recordNames: [props.hostedZoneName],
+        targetDomain: props.targetDomainName,
+        zone: HostedZone,
+      });
+    }
+  }
