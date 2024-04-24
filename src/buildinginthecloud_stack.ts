ef4040ace4fae6b756207065a13484cc8cf533ba
Yvo van Zee <yvo.vanzee@xebia.com>
2024-04-24 12:59:20 +0200

    deploy redirect

--- src/buildinginthecloud_stack.ts
+++ src/buildinginthecloud_stack.ts
@@ -1,4 +1,4 @@
-import { Stack, StackProps, aws_route53 } from 'aws-cdk-lib';
+import { Stack, StackProps, aws_route53, aws_route53_patterns } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
 export interface BuildingInTheCloudStackProps extends StackProps {
@@ -10,14 +10,14 @@ export class BuildingInTheCloudStack extends Stack {
   constructor(scope: Construct, id: string, props: BuildingInTheCloudStackProps) {
     super(scope, id, props);
 
-    new aws_route53.HostedZone(this, 'HostedZone', {
+    const HostedZone = new aws_route53.HostedZone(this, 'HostedZone', {
       zoneName: props.hostedZoneName,
     });
 
-    // new aws_route53_patterns.HttpsRedirect(this, 'Redirect', {
-    //   recordNames: [props.hostedZoneName],
-    //   targetDomain: props.targetDomainName,
-    //   zone: HostedZone,
-    // });
+    new aws_route53_patterns.HttpsRedirect(this, 'Redirect', {
+      recordNames: [props.hostedZoneName],
+      targetDomain: props.targetDomainName,
+      zone: HostedZone,
+    });
   }
 }
